df65c5d8f803bf20149af8e8aea6ec7934effc87
Mateusz <mathew984@gmail.com>
2017-01-23 21:39:32 +0100

    P-003

--- src/main/java/mainClass/Main.java
+++ src/main/java/mainClass/Main.java
@@ -25,27 +25,6 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-//        MpkConnector mpk = new MpkConnector();
-//        try{
-//            mpk.downloadDatabase(mpk.getDatabaseUrl(), System.getProperty("user.home")+System.getProperty("file.separator", "\\")+".appDontLose"+System.getProperty("file.separator", "\\")+"database.sqlite");
-//        }catch (IOException ex){
-//            ex.printStackTrace();
-//        }
-//        System.out.println("DB ADRESS: " + System.getProperty("user.home")+System.getProperty("file.separator", "\\")+".appDontLose"+System.getProperty("file.separator", "\\")+"database.sqlite");
-//        StopDao stopDao = new StopDaoImpl(System.getProperty("user.home")+System.getProperty("file.separator", "\\")+".appDontLose"+System.getProperty("file.separator", "\\")+"database.sqlite");
-//        LineDao lineDao = new LineDaoImpl(
-//                System.getProperty("user.home")+System.getProperty("file.separator", "\\")+".appDontLose"+System.getProperty("file.separator", "\\")+"database.sqlite",
-//                stopDao
-//        );
-//        lineDao.initialize();
-//
-//        Connection output = (new Search(lineDao, stopDao)).searchConnection("Norymberska", "Solvay", 3);
-//        System.out.println(output.getDescription() + " Z: " + output.getConnectionParts().get(0).getStopsList().get(0).getName() +
-//                ", przez: " + output.getConnectionParts().get(1).getStopsList().get(0).getName() +
-//                ", do " +  output.getConnectionParts().get(1).getStopsList().get( output.getConnectionParts().get(1).getStopsList().size()-1).getName() +
-//                ". Liniami: " + output.getConnectionParts().get(0).getLine().getName() + "-" + output.getConnectionParts().get(0).getLine().getLastStop().getName() +
-//                " -- " + output.getConnectionParts().get(1).getLine().getName() + "-" + output.getConnectionParts().get(1).getLine().getLastStop().getName()
-//        );
     }
 
     public static void main(String[] args) {
